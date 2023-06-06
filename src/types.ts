7c5111e91be11bd82ec4b592d03ba834150abc45
Guide Fari <guide.fari@flexperto.com>
2023-06-06 19:20:58 +0200

    chore: ForecastResponse types

--- src/types.ts
+++ src/types.ts
@@ -62,15 +62,68 @@ export interface Sys {
   sunset: number;
 }
 
-export interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
 export interface Wind {
   speed: number;
   deg: number;
   gust: number;
+}
+
+export interface ForecastResponse {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: List[];
+  city: City;
+}
+
+export interface City {
+  id: number;
+  name: string;
+  coord: Coord;
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+export interface List {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number;
+  sys: Sys;
+  dt_txt: Date;
+}
+
+export interface Sys {
+  pod: Pod;
+}
+
+export enum Pod {
+  D = "d",
+  N = "n",
+}
+
+export interface Weather {
+  id: number;
+  main: MainEnum;
+  description: Description;
+  icon: string;
+}
+
+export enum Description {
+  BrokenClouds = "broken clouds",
+  ClearSky = "clear sky",
+  FewClouds = "few clouds",
+  OvercastClouds = "overcast clouds",
+  ScatteredClouds = "scattered clouds",
+}
+
+export enum MainEnum {
+  Clear = "Clear",
+  Clouds = "Clouds",
 }
